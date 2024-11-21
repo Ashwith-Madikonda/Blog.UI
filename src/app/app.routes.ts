a01978b0d7f44634dd669e7f5344537b1da55911
Ashwith Madikonda <ashwithreddy56@gmail.com>
2024-11-20 22:05:42 -0500

    portfolio page setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: PortfolioComponent,
+    },
+    {
+        path: "Ashwith-Madikonda",
+        component: PortfolioComponent,
+    },
+    {
+        path: "**",
+        redirectTo: ""
+    }
+];
