2eacd0bb0b4d763ae9ffbbdfa76fe686f2d544b9
Ashwith Madikonda <ashwithreddy56@gmail.com>
2024-11-21 22:53:03 -0500

    cover details and about added

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
 
+  constructor() {
+    this.skillsData = [
+      {skillName: "C#", image:'CSharp.png'},
+      {skillName: "Java Script", image:"js.png"},
+      {skillName: ".NET", image:"DotNet.jpg"},
+      {skillName: "Angular", image:"Angular.png"},
+      {skillName: "HTML", image:"html.png"},
+      {skillName: "CSS", image:"css.png"},    
+      {skillName: "Azure", image:"azure.png"},
+      {skillName: "SQL Server", image:"sql.png"},
+      {skillName: "Git", image:"git.png"},
+    ];
+  }
+  skillsData: any [] = [];
+  coverPhoto = '/public/ashwith.png';
+  
 }
