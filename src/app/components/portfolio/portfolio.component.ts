1b0bacafcb99eb56fbcc9b45229d5e113cbaea27
Ashwith Madikonda <ashwithreddy56@gmail.com>
2024-11-28 23:12:07 -0500

    experience and testimonial section added

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -22,8 +22,17 @@ export class PortfolioComponent {
       {skillName: "SQL Server", image:"sql.png"},
       {skillName: "Git", image:"git.png"},
     ];
+    this.experienceData = [
+      {title: "Information Security Analyst", year:"2024",organization:"First Canadian Title", location:"Greater Toronto Area, Canada"},
+      {title: "Senior Software Engineer", year:"2023",organization:"Optum - United Health Group", location:"Hyderabad, India"},
+      {title: "Senior Consultant I", year:"2022",organization:"Neudesic - an IBM Company", location:"Hyderabad, India"},
+      {title: "Consultant II", year:"2021",organization:"Neudesic - an IBM Company", location:"Hyderabad, India"},
+      {title: "Consultant I", year:"2020",organization:"Neudesic - an IBM Company", location:"Hyderabad, India"},
+      {title: "Associate Consultant", year:"2018",organization:"Neudesic - an IBM Company", location:"Bangalore, India"}
+    ];
   }
   skillsData: any [] = [];
+  experienceData: any [] = [];
   coverPhoto = '/public/ashwith.png';
   
 }
